435d05ea311674f57dc35451d8847076ce7fd6f4
UnCompa <brandonddxd@gmail.com>
2024-09-07 16:00:36 -0500

    feat: Gestion de materias

---
+++ gestionMaterias_Brandon.js
@@ -0,0 +1,76 @@
+let materias = []
+/**
+ * txtNombreMateria
+ * txtCodigo
+ * txtDescripcion
+ */
+
+const crearMateria = () => {
+    let materia = {}
+    let nombreMateria = recuperarTexto("txtNombreMateria")
+    let codigo = recuperarTexto("txtCodigo")
+    let descripcion = recuperarTexto("txtDescripcion")
+    materia.nombreMateria = nombreMateria;
+    materia.codigo = codigo;
+    materia.descripcion = descripcion;
+    return materia
+}
+
+const agregarMateria = () => {
+    let materia = crearMateria()
+    materias.push(materia)
+}
+
+const refrescarMaterias = () => {
+    let tabla = document.getElementById("mostrarTabla")
+
+    let contenidoTabla = `
+    <table>
+        <tr>
+            <th>
+            NOMBRE
+            </th>
+            <th>
+            CODIGO
+            </th>
+            <th>
+            DESCRIPCION
+            </th>
+        </tr>
+    `
+
+    for (let i = 0; i < materias.length; i++) {
+        const materia = materias[i];
+        contenidoTabla += `
+         <tr>
+            <td>
+            ${materia.nombreMateria}
+            </td>
+            <td>
+            ${materia.codigo}
+            </td>
+            <td>
+            ${materia.descripcion}
+            </td>
+        </tr>
+        `
+    }
+    contenidoTabla += ` </table>`
+    console.log(contenidoTabla);
+    tabla.innerHTML = contenidoTabla
+}
+const buscarMateria = (codigo) => {
+    let materiaEncontrada = null;
+    let txtCodigo = recuperarTexto("txtBuscar")
+    for (let i = 0; i < materias.length; i++) {
+        const materia = materias[i];
+        if(materia.codigo == txtCodigo){
+            materiaEncontrada = materia
+        }
+    } 
+    if(materiaEncontrada != null) {
+        alert("Materia encontrada")
+    } else {
+        alert("Materia no encontrada")
+    }
+}
